b5e2aa09d25bfc1a3e009ad89d092b9987df3784
Filipe Silva <emanuel_filipe@yahoo.com.br>
2024-08-20 15:25:17 -0300

    creating datatables

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # knowledge
+
+##  backend
+npm start
+
+## knex
+usado para acessar o banco de dados postgres
+
+## starting mongod
+- acessing the path on therminal: C:\Program Files\MongoDB\Server\7.0\bin
+execute: mongod
+
+## create table
+dentro de backend:
+
+npx knex migrate:make create_table_users
--- versao-inicial/backend/index.js
+++ versao-inicial/backend/index.js
@@ -1,5 +1,15 @@
 const express = require('express')
 const app = express()
+const consign = require('consign')
+const db = require('./config/db')
+
+app.db = db
+
+// configurando o consign para ler todos esses arquivos
+consign().then('./config/middlewares.js')
+         .then('./api')   
+         .then('./config/routes.js')
+         .into(app)
 
 app.listen(3000, () => {
     console.log('Backend executando...')
