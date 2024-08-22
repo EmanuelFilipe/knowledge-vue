6917d1ccb35a3f5f4b5a1304f904e240e144984e
Filipe Silva <emanuel_filipe@yahoo.com.br>
2024-08-22 10:39:26 -0300

    ajustes no backend

--- versao-inicial/backend/api/user.js
+++ versao-inicial/backend/api/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt-nodejs')
+
 module.exports = app => {
     const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation
 
@@ -38,7 +39,6 @@ module.exports = app => {
             app.db('users')
                 .update(user)
                 .where({ id: user.id })
-                .whereNull('deletedAt')
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         } else {
@@ -53,10 +53,17 @@ module.exports = app => {
     const get = (req, res) => {
         app.db('users')
             .select('id', 'name', 'email', 'admin')
-            .whereNull('deletedAt')
             .then(users => res.json(users))
             .catch(err => res.status(500).send(err))
     }
+    const getById = (req, res) => {
+        app.db('users')
+            .select('id', 'name', 'email', 'admin')
+            .where({ id: req.params.id })
+            .first()
+            .then(user => res.json(user))
+            .catch(err => res.status(500).send(err))
+    }
 
-    return { save, get }
+    return { save, get, getById }
 }
--- versao-inicial/backend/index.js
+++ versao-inicial/backend/index.js
@@ -6,11 +6,13 @@ const db = require('./config/db')
 app.db = db
 
 // configurando o consign para ler todos esses arquivos
-consign().then('./config/middlewares.js')
-         .then('./api/validation.js')   
-         .then('./api')   
-         .then('./config/routes.js')
-         .into(app)
+consign()
+        .include('./config/passport.js')
+        .then('./config/middlewares.js')
+        .then('./api/validation.js')   
+        .then('./api')   
+        .then('./config/routes.js')
+        .into(app)
 
 app.listen(3000, () => {
     console.log('Backend executando...')
